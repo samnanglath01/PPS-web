9122167e549529c519c89d60f53208a143f58537
saochanny <138644257+saochanny@users.noreply.github.com>
2023-07-06 17:34:35 +0700

    my commit

--- src/component/header/Header.jsx
+++ src/component/header/Header.jsx
@@ -11,7 +11,10 @@ export default function Header(){
                 </div>
                 <div className="col-xxl-10 col-xl-10 col-lg-10 col-md-9 col-sm-10 col-10 menuBar">
                     <div className="menu">
-
+                        <span>Accueil</span>
+                        <span>Projets</span>
+                        <span>Services</span>
+                        <span>Contact</span>
                     </div>
                     <div className="appointment">
                         <span>
