51e23dd5cf6ff8ebca562dde075ac001fd9f27b5
saochanny <138644257+saochanny@users.noreply.github.com>
2023-07-06 22:38:51 +0700

    Commit Menu Bar

--- src/component/header/Header.jsx
+++ src/component/header/Header.jsx
@@ -1,29 +1,36 @@
-import React from "react";
+import React, { useState } from "react";
 
 import 'bootstrap/dist/css/bootstrap.css';
 import './header.css';
 export default function Header(){
+    const [showMenu,setShowMenu]=useState(false);
+    const handleShowMenu=()=>{
+        setShowMenu(!showMenu);
+    }
+    const handleHideMenu=()=>{
+        setShowMenu(false);
+    }
     return(
         <div className="container header">
             <div className="row bar">
                 <div className="col-xxl-2 col-xl-2 col-lg-2 col-md-2 col-sm-3 col-3 logo">
                     <img src="Assets/PPS_logo.png" alt="logo" />
                 </div>
-                <div className="col-xxl-10 col-xl-10 col-lg-10 col-md-10 col-sm-10 col-10 menuBar">
+                <div className="col-xxl-10 col-xl-10 col-lg-10 col-md-10 col-sm-12 col-12 menuBar" id={`${showMenu ? 'show':''}`}>
                     <div className="menu">
-                        <span>Accueil</span>
-                        <span>Projets</span>
-                        <span>Services</span>
-                        <span>Contact</span>
+                        <span onClick={handleHideMenu}>Accueil</span>
+                        <span onClick={handleHideMenu}>Projets</span>
+                        <span onClick={handleHideMenu}>Services</span>
+                        <span onClick={handleHideMenu}>Contact</span>
                     </div>
                     <div className="appointment">
-                        <span>
-                            Prendre rendez-vous <i class="fa-solid fa-phone fa-beat-fade fa-sm"></i>
+                        <span onClick={handleHideMenu}>
+                            Prendre rendez-vous <i class="fa-solid fa-phone"></i>
                         </span>
                     </div>
                 </div>
                 <div className="btnMenu col-sm-9 col-9">
-                    <i class="fa-solid fa-bars"></i>
+                    <i class="fa-solid fa-bars" onClick={handleShowMenu}></i>
                 </div>
             </div>
         </div>
