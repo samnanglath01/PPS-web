1ca10c646a43e331d8a6dfc5f49b5a08724df23d
saochanny <138644257+saochanny@users.noreply.github.com>
2023-07-06 16:55:57 +0700

    my commit

--- src/component/header/Header.jsx
+++ src/component/header/Header.jsx
@@ -1,18 +1,26 @@
 import React from "react";
-import './header.css';
+
 import 'bootstrap/dist/css/bootstrap.css';
+import './header.css';
 export default function Header(){
     return(
-        <div className="container-fluid header">
-            <div className="row menuBar">
-                <div className="col-xxl-2 col-xl-2 col-lg-2 col-md-2 logo">
-                    <img src="Assets/PPS_logo.png" alt="" />
+        <div className="container header">
+            <div className="row bar">
+                <div className="col-xxl-2 col-xl-2 col-lg-2 col-md-3 col-sm-2 col-2 logo">
+                    <img src="Assets/PPS_logo.png" alt="logo" />
                 </div>
-                <div className="col-xxl-8 col-xl-8 col-lg-8 col-md-8 menuBar">
-                    1
+                <div className="col-xxl-10 col-xl-10 col-lg-10 col-md-9 col-sm-10 col-10 menuBar">
+                    <div className="menu">
+
+                    </div>
+                    <div className="appointment">
+                        <span>
+                            Prendre rendez-vous <i class="fa-solid fa-phone fa-beat-fade fa-sm"></i>
+                        </span>
+                    </div>
                 </div>
                 <div className="btnMenu">
-                    
+
                 </div>
             </div>
         </div>
