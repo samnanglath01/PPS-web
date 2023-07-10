f62749fba24a2dbbfaeb6530d70d25d293e0f966
saochanny <138644257+saochanny@users.noreply.github.com>
2023-07-10 18:05:21 +0700

    commit

--- src/component/footer/Footer.jsx
+++ src/component/footer/Footer.jsx
@@ -1,8 +1,10 @@
 import './footer.css';
 export default function Footer(){
     return(
-        <div className="container-fluid d-flex justify-content-between footer">
-            .
+        <div className="container-fluid d-flex justify-content-between footer w-100">
+            <div className="row content">
+                
+            </div>
             
         </div>
     )
